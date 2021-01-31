acad350639fb222b7811f7fc8246903e638cc6c6
ifbiu <biuvip@163.com>
2021-01-31 12:45:55 +0800

    热销推荐

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,18 +3,21 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <recommend></recommend>
   </div>
 </template>
 <script>
 import HomeHeader from './components/Header.vue'
 import HomeSwiper from './components/Swiper.vue'
 import HomeIcons from './components/Icons.vue'
+import Recommend from './components/Recommend'
 export default{
   name: 'home',
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    Recommend
   }
 }
 </script>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="(item,index) in recommendList" :key="index">
+        <img :src="item.imgUrl" alt="" class="item-img">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Recommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1511/d2/d2aec2dfc5aa771290.water.jpg_200x200_69e4a80d.jpg',
+          title: '南山滑雪场',
+          desc: '北京南山滑雪场--日场全天含雪具'
+        },
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1511/d2/d2aec2dfc5aa771290.water.jpg_200x200_69e4a80d.jpg',
+          title: '南山滑雪场',
+          desc: '北京南山滑雪场--日场全天含雪具'
+        },
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1511/d2/d2aec2dfc5aa771290.water.jpg_200x200_69e4a80d.jpg',
+          title: '南山滑雪场',
+          desc: '北京南山滑雪场--日场全天含雪具'
+        },
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1511/d2/d2aec2dfc5aa771290.water.jpg_200x200_69e4a80d.jpg',
+          title: '南山滑雪场',
+          desc: '北京南山滑雪场--日场全天含雪具'
+        },
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1511/d2/d2aec2dfc5aa771290.water.jpg_200x200_69e4a80d.jpg',
+          title: '南山滑雪场',
+          desc: '北京南山滑雪场--日场全天含雪具'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "../../../assets/styles/mixins.styl"
+  .title
+    margin-top .2rem
+    line-height .8rem
+    background #eee
+    text-indent .2
+  .item
+    overflow hidden
+    display flex
+    height 1.9rem
+    .item-img
+      width 1.7rem
+      height 1.7rem
+      padding .1rem
+    .item-info
+      flex 1
+      padding .1rem
+      min-width 0
+      .item-title
+        line-height .54rem
+        font-size .32rem
+        ellipsis()
+      .item-desc
+        line-height .4rem
+        color #ccc
+        ellipsis()
+      .item-button
+        margin-top .2rem
+        background #ff9300
+        padding 0 .2rem
+        border-radius .06rem
+        color #fff
+</style>
