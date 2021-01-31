7bc6f566cc050c5fd2dbb1be48b3e05ffe82b0b1
ifbiu <biuvip@163.com>
2021-02-01 00:04:26 +0800

    城市列表布局

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="list">
+    <div class="area">
+      <div class="title border-topbottom">当前城市</div>
+      <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">热门城市</div>
+      <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">上海</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">广州</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">天津</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉善</div>
+        <div class="item border-bottom">阿拉善</div>
+        <div class="item border-bottom">阿拉善</div>
+        <div class="item border-bottom">阿拉善</div>
+        <div class="item border-bottom">阿拉善</div>
+        <div class="item border-bottom">阿拉善</div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉善</div>
+        <div class="item border-bottom">阿拉善</div>
+        <div class="item border-bottom">阿拉善</div>
+        <div class="item border-bottom">阿拉善</div>
+        <div class="item border-bottom">阿拉善</div>
+        <div class="item border-bottom">阿拉善</div>
+      </div>
+    </div><div class="area">
+    <div class="title border-topbottom">A</div>
+    <div class="item-list">
+      <div class="item border-bottom">阿拉善</div>
+      <div class="item border-bottom">阿拉善</div>
+      <div class="item border-bottom">阿拉善</div>
+      <div class="item border-bottom">阿拉善</div>
+      <div class="item border-bottom">阿拉善</div>
+      <div class="item border-bottom">阿拉善</div>
+    </div>
+  </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+      <div class="item-list">
+        <div class="item border-bottom">阿拉善</div>
+        <div class="item border-bottom">阿拉善</div>
+        <div class="item border-bottom">阿拉善</div>
+        <div class="item border-bottom">阿拉善</div>
+        <div class="item border-bottom">阿拉善</div>
+        <div class="item border-bottom">阿拉善</div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityList'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "../../../assets/styles/variables.styl"
+  .border-topbottom
+    &:before
+      border-color #ccc
+    &:after
+      border-color #ccc
+  .border-bottom
+    &:after
+      border-color #ccc
+  .list
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    .title
+      line-height .44rem
+      background #eee
+      padding-left .2rem
+      color #666
+      font-size .26rem
+    .button-list
+      overflow hidden
+      padding .1rem .6rem .1rem .1rem
+      .button-wrapper
+        float left
+        width 33.33%
+        .button
+          text-align center
+          padding .1rem 0
+          margin .1rem
+          border .02rem solid #ccc
+          border-radius .06rem
+    .item-list
+      .item
+        line-height .76rem
+        color #666
+        padding-left .2rem
+</style>
